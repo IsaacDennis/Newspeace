27ab45bed0d5d306570b4ea5c6377ce8850694ea
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-06-21 23:42:02 -0300

    Criação do OrganizationService

--- src/app/model/organization.ts
+++ src/app/model/organization.ts
@@ -1,19 +1,18 @@
 export interface Organization {
-  activeProjects: number;
-  totalProjects: number;
-  addressLine1: string;
-  addressLine2: string;
-  city: string;
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  countries: {}; // Dentro do countries, existe o atributo "country", que contém de fato um array com os países de atuação ou somente país
-  country: string; // País de origem da instituição, e não de atuação
-  id: number;
-  iso3166CountryCode: string;
-  mission: string;
-  name: string;
-  postal: number;
-  state: string;
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  themes: {}; // Que nem os countries: existe o atributo "theme", que contém um array de temas ou somente um objeto
-  url: string;
+  activeProjects: number,
+  totalProjects: number,
+  addressLine1: string,
+  addressLine2: string,
+  city: string,
+  countries: {}, // Dentro do countries, existe o atributo "country", que contém de fato um array com os países de atuação ou somente país
+  country: string, // País de origem da instituição, e não de atuação
+  id: number,
+  iso3166CountryCode: string,
+  mission: string,
+  name: string,
+  postal: number,
+  state: string,
+  themes: {}, // Que nem os countries: existe o atributo "theme", que contém um array de temas ou somente um objeto
+  url: string,
+
 }
---
+++ src/app/services/organization.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
+import { Organization } from '../model/organization';
+import * as jsonOrgs from '../../assets/active_organizations.json';
+@Injectable({
+  providedIn: 'root'
+})
+// TODO: transição para a API Filesystem do Capacitor
+export class OrganizationService {
+  organizations: Organization[];
+  constructor() {
+    this.organizations = (jsonOrgs as any).organizations.organization;
+  }
+  // O parâmetro organizations, por padrão, é o atributo "organizations" da classe. Pode-se passar um array de organizações que já foram filtrados por outro método. Ex.: filtrar as organizações que atuam na Índia e então passá-las como parâmetro para "getOrganizationsByTheme"
+  getOrganizationsByCountry(countryName, organizations = this.organizations): Organization[] {
+    return organizations.filter(org => this.getOrganizationCountries(org).map(country => country.name).includes(countryName));
+  }
+  getOrganizationsByTheme(themeName, organizations = this.organizations): Organization[] {
+    return organizations.filter(org => this.getOrganizationThemes(org).map(theme => theme.name).includes(themeName));
+  }
+  getOrganizationsByCountryISO(isoName, organizations = this.organizations): Organization[] {
+    return organizations.filter(org => this.getOrganizationCountries(org).map(country => country.iso3166CountryCode).includes(isoName));
+  }
+  /*
+      Retorna um array com objeto(s) que representa(m) um país:
+      Exemplo:
+      {
+         iso3166CountryCode: 'US',
+         name: 'United States'
+      }
+      */
+  getOrganizationCountries(organization): any[] {
+    const countries = organization.countries.country; // Apesar de estar no plural, countries pode ser um array de objetos ou um objeto único
+    if (countries != null) {
+      return countries instanceof Array ? countries : [countries];
+    }
+    return [];
+  }
+  /*
+      Exemplo de objeto 'theme':
+      {
+         id: 'eco',
+         name: 'Ecosystem Restoration'
+      }
+      */
+  getOrganizationThemes(organization): any[] {
+    const themes = organization.themes.theme // Ocorre o mesmo que a variável countries
+    if (themes != null) {
+      return themes instanceof Array ? themes : [themes];
+    }
+    return [];
+  }
+}
