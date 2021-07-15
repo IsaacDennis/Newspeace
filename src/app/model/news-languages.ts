a7e964dea40500e437310961bb0b78a164b9e22c
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-07-14 23:54:26 -0300

    Implementação de pesquisa com várias linguagens

---
+++ src/app/model/news-languages.ts
@@ -0,0 +1,13 @@
+/*
+Esta enum deve ser usada para a seleção de linguagens das notícias. Consiste em duas strings separadas
+por : -> o primeiro, o código da linguagem, e o segundo a string de pesquisa para ser utilizada na API (ver news.service.ts)
+*/
+export enum NewsLanguage {
+  PT = 'pt:humanitário',
+  DE = 'de:humanitärd', // Alemão
+  EN = 'en:humanitarian', // Inglês
+  FR = 'fr:humanitaire', // Francês
+  IT = 'it:umanitario', // Italiano
+  NL = 'nl:humanitair', //Holandês
+  RU = 'ru:gumanitarnyy', // Russo
+}
