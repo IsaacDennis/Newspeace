4929a94f1b667a7c3b9fc8b0733780d04681b042
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-07-19 21:37:57 -0300

    Tipo Country

---
+++ src/app/model/country.ts
@@ -0,0 +1,20 @@
+export interface Country {
+  continent: string, // Acrônimo do continente do país. Ex.: AF = Africa
+  continentName: string,
+  capital: string,
+  languages: string, //Linguagens do país, separadas por vírgula (ex.: languages: "pt-br,en,fr")
+  areaInSqKm: string, // Área do país em km ao quadrado
+  countryCode: string,
+  countryName: string,
+  currencyCode: string,
+  east: number,
+  fipsCode: string,
+  geonameId: number,
+  isoAlpha3: string,
+  isoNumeric: string,
+  north: number,
+  population: string,
+  postalCodeFormat: string,
+  south: number,
+  west: number
+}
