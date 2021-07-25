806c6e9b49f8fb68c27562e7e908d4329584654a
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-07-24 22:20:17 -0300

    TESTE: Detecção de país

--- src/app/news-modal/news-modal.page.ts
+++ src/app/news-modal/news-modal.page.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MenuController, ModalController } from '@ionic/angular';
 import { News } from '../model/news';
+import { GeonamesService } from '../services/geonames.service';
 import { OrganizationService } from '../services/organization.service';
 
 @Component({
@@ -10,9 +11,12 @@ import { OrganizationService } from '../services/organization.service';
 })
 export class NewsModalPage implements OnInit {
   @Input() news: News;
-  constructor(private modalController: ModalController, private menuController: MenuController, private os: OrganizationService) { }
+  constructor(private modalController: ModalController, private menuController: MenuController, private os: OrganizationService, private geonames: GeonamesService) {
+
+  }
 
   ngOnInit() {
+    console.log(this.geonames.findCountriesInNews(this.news))
   }
   closeNewsModal(){
     this.modalController.dismiss();
