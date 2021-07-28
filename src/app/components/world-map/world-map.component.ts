64643cc6aeae94a4dcf5784ae3fd0abbd1251b5a
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-07-28 12:15:42 -0300

    Lógica do mapa

--- src/app/components/world-map/world-map.component.ts
+++ src/app/components/world-map/world-map.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSlides, ModalController } from '@ionic/angular';
+import { NewsCountryModalPage } from 'src/app/modals/news-country-modal/news-country-modal.page';
+import { News } from 'src/app/model/news';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-world-map',
@@ -6,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./world-map.component.scss'],
 })
 export class WorldMapComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {}
-
+  @ViewChild(IonSlides) slides: IonSlides;
+  sliderOpts = {
+    allowTouchMove: false
+  };
+  constructor(private ns: NewsService, private modalController: ModalController) { }
+  async presentNewsCountryModal(event){
+    const countryName = this.getCountryName(event);
+    const newsArray = this.getNewsInCountry(countryName);
+    const newsCountryModal = await this.modalController.create({
+      component: NewsCountryModalPage,
+      componentProps: { newsArray }
+    });
+    return await newsCountryModal.present();
+  }
+  ngOnInit() { }
+  getCountryName(event): string{
+    const countryName = event.srcElement.attributes.id.nodeValue;
+    return countryName.split("-").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+  }
+  getNewsInCountry(countryName: string){
+    return this.ns.getNewsByCountry(countryName);
+  }
+  async zoom(zoomIn: boolean) {
+    const slider = await this.slides.getSwiper();
+    const zoom = slider.zoom;
+    if (zoomIn === true) {
+      slider.updateSize();
+      zoom.in();
+    } else {
+      slider.updateSize();
+      zoom.out();
+    }
+  }
 }
