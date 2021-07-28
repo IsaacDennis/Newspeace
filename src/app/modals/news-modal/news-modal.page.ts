3130d5cdc8795e5af9db0305b1a74c4d704cf6cb
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-07-27 22:14:16 -0300

    Menu de organizações

--- src/app/modals/news-modal/news-modal.page.ts
+++ src/app/modals/news-modal/news-modal.page.ts
@@ -1,9 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MenuController, ModalController } from '@ionic/angular';
+import { Organization } from 'src/app/model/organization';
 import { News } from '../../model/news';
 import { GeonamesService } from '../../services/geonames.service';
 import { NewsService } from '../../services/news.service';
 import { OrganizationService } from '../../services/organization.service';
+import { OrgsModalPage } from '../orgs-modal/orgs-modal.page';
 
 @Component({
   selector: 'app-news-modal',
@@ -12,13 +14,20 @@ import { OrganizationService } from '../../services/organization.service';
 })
 export class NewsModalPage implements OnInit {
   @Input() news: News;
+    organizations: Organization[] //Organizações relacionadas a esta notícia
 
   constructor(private modalController: ModalController, private menuController: MenuController, private os: OrganizationService, private geonames: GeonamesService, private newsService: NewsService) {
 
   }
 
   ngOnInit() {
-    console.log(this.geonames.findCountriesInNews(this.news))
+    let orgsInCountry = [];
+    if (this.news.countryName != ""){
+      orgsInCountry = this.os.getOrganizationsByCountry(this.news.countryName);
+      if (this.news.themes != []){
+        this.organizations = this.os.getOrganizationsByTheme(this.news.themes, orgsInCountry);
+      }
+    }
   }
   closeNewsModal(){
     this.modalController.dismiss();
@@ -28,5 +37,12 @@ export class NewsModalPage implements OnInit {
   }
   closeOrgsMenu() {
     this.menuController.close();
+  }
+  async presentOrgsModal(org: Organization){
+    const orgsModal = await this.modalController.create({
+      component: OrgsModalPage,
+      componentProps: { org }
+    });
+    return await orgsModal.present();
   }
 }
