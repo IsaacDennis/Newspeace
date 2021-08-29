f8201bc81acea208644d6a9d1bd43ab137b018c9
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-08-29 12:04:59 -0300

    Apresentação do WelcomeModal na inicialização do aplicativo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { WelcomeModalPage } from './modals/welcome-modal/welcome-modal.page';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit{
+  constructor(private modalController: ModalController) {}
+  ngOnInit(){
+    this.presentWelcomeModal();
+  }
+  async presentWelcomeModal(){
+    const welcomeModal = await this.modalController.create({
+      component: WelcomeModalPage
+    });
+    return await welcomeModal.present();
+  }
 }
