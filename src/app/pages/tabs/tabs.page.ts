b080a0556c78542cc270cd84db8455c2225e9667
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-08-29 12:08:11 -0300

    Desabilitar mapa-múndi enquanto as notícias não forem carregadas

--- src/app/pages/tabs/tabs.page.ts
+++ src/app/pages/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-tabs',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabs.page.scss'],
 })
 export class TabsPage implements OnInit {
-
-  constructor() { }
+  worldMapEnabled: boolean;
+  constructor(private ns: NewsService) {
+  }
 
   ngOnInit() {
+    this.worldMapEnabled = false;
+    this.ns.news.subscribe(news => {
+      if (news.length > 0){
+        this.worldMapEnabled = true;
+      }
+    });
   }
 
 }
