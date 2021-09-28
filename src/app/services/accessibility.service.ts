042b9017cd25986205a696281064ae7db392c651
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-09-28 15:34:05 -0300

    Adição dos métodos de acessibilidade visual por voz

--- src/app/services/accessibility.service.ts
+++ src/app/services/accessibility.service.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import { ScreenReader } from '@capacitor/screen-reader';
+import { News } from '../model/news';
+
 @Injectable({
   providedIn: 'root'
 })
 export class AccessibilityService {
 
   constructor() { }
-
+  // Checa se o recurso de fala (para deficientes visuais) está disponível no dispositivo
+  async getVoiceAvailability(){
+    const { value } = await ScreenReader.isEnabled();
+    return value;
+  }
+  async speak(news: News){
+    await ScreenReader.speak({value: news.title});
+    await ScreenReader.speak({value: news.description});
+  }
 }
