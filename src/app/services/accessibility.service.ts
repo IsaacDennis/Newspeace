91ee4475a7d998fec9ef3f03d4d28ac5c20f75f7
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-09-29 18:23:04 -0300

    Substituição do ScreenReader pelo TextToSpeech

--- src/app/services/accessibility.service.ts
+++ src/app/services/accessibility.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { News } from '../model/news';
 import { TextToSpeech } from '@capacitor-community/text-to-speech';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,6 +13,19 @@ export class AccessibilityService {
 
   }
   async speak(news: News){
-
+    const bcpCode = this.getBcpCode(news);
+    await TextToSpeech.speak({
+      text: news.title,
+      lang: bcpCode
+    });
+  }
+  // Retorna o código BCP 47 da linguagem da notícia. Ex.: pt -> pt_BR (usar português brasileiro em vez de português de portugal)
+  // TODO: Adicionar mais linguagens
+  getBcpCode(news: News){
+    const languageCode = news.languageCode;
+    if (languageCode === 'pt'){
+      return 'pt-BR';
+    }
+    return 'en-US';
   }
 }
