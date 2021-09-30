d001eca621574f8949c44aaeae98829963d4adba
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-09-30 18:14:56 -0300

    Adição das preferências de acessibilidade com síntese de fala

--- src/app/services/preferences.service.ts
+++ src/app/services/preferences.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
-
+import { Storage } from '@capacitor/storage';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class PreferencesService {
 
-  constructor() { }
+  ttsAccessibility: BehaviorSubject<boolean> = new BehaviorSubject(false); // Acessibilidade através de síntese de fala (text-to-speech)
+  constructor() {
+    const ttsEnabled = this.readTtsAccessibility().then(value => {
+      this.ttsAccessibility.next(value);
+    });
+  }
+  async readTtsAccessibility(): Promise<boolean> {
+    const { value } = await Storage.get({ key: 'tts-accessibility' });
+    const ttsBoolean = JSON.parse(value);
+    if (ttsBoolean == null) return false;
+
+    return ttsBoolean;
+  }
+  async setTtsAccessibility(ttsBoolean: boolean){
+    this.ttsAccessibility.next(ttsBoolean);
+    await Storage.set({
+      key: 'tts-accessibility',
+      value: JSON.stringify(ttsBoolean)
+    });
+  }
 }
