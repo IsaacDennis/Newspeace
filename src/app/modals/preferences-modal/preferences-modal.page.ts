4c2a408fa38c0362dd0497ad0313d82b502b73dc
Isaac Dennis <isaac.dennis.moreira@gmail.com>
2021-10-03 22:14:13 -0300

    Alerta ao ativar síntese de fala

--- src/app/modals/preferences-modal/preferences-modal.page.ts
+++ src/app/modals/preferences-modal/preferences-modal.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PreferencesService } from '../../services/preferences.service';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
 @Component({
   selector: 'app-preferences-modal',
   templateUrl: './preferences-modal.page.html',
@@ -8,15 +8,28 @@ import { ModalController } from '@ionic/angular';
 })
 export class PreferencesModalPage implements OnInit {
   ttsEnabled: boolean;
-  constructor(private preferences: PreferencesService, private modalController: ModalController) { }
+  constructor(private preferences: PreferencesService, private modalController: ModalController, private alertController: AlertController) { }
 
   ngOnInit() {
     this.preferences.ttsAccessibility.subscribe(value => this.ttsEnabled = value);
   }
   toggleTts(){
-    this.preferences.setTtsAccessibility(!this.ttsEnabled);
+    const toggledTts = !this.ttsEnabled;
+    if (toggledTts){
+      this.presentLanguageAlert();
+    }
+
+    this.preferences.setTtsAccessibility(toggledTts);
   }
   closeModal(){
     this.modalController.dismiss();
+  }
+  async presentLanguageAlert(){
+    const alert = await this.alertController.create({
+      header: 'ATENÇÃO:',
+      message: 'Caso nenhum som seja reproduzido, é necessário instalar os pacotes de síntese de fala nas configurações do seu dispositivo.',
+      buttons: ['Prosseguir']
+    });
+    await alert.present();
   }
 }
